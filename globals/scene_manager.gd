acd5188e59736254e0aa6e6ca64957b8394161f0
Unain <vanstoel@gmail.com>
2023-10-02 14:22:53 +0200

    Added Load Prev Level Debug Key

--- globals/scene_manager.gd
+++ globals/scene_manager.gd
@@ -6,4 +6,16 @@ var current_level: PackedScene
 func go_to_next_level() -> void:
 	current_level_nr += 1
 	current_level = load("res://scenes/levels/level%s.tscn" % current_level_nr)
+	if current_level:
+		get_tree().change_scene_to_packed(current_level)
+	else:
+		printerr("No level found for level nr: %s!" % current_level_nr)
+
+func go_to_prev_level() -> void:
+	if current_level_nr == 1:
+		printerr("No level before level 1!")
+		return
+
+	current_level_nr -= 1
+	current_level = load("res://scenes/levels/level%s.tscn" % current_level_nr)
 	get_tree().change_scene_to_packed(current_level)
