fdd2f13665861036662fa695ffd8d24c2189d323
Unain <vanstoel@gmail.com>
2023-10-05 16:30:50 +0200

    Added Allowed to Spawn Powerup to bricktype

--- scenes/bricks/brick_type.gd
+++ scenes/bricks/brick_type.gd
@@ -3,6 +3,7 @@ class_name BrickType
 
 @export var name: String = "Red"
 @export var points: int = 90
-@export var hits_to_destroy: int = 1	# TODO: Silver = +1 every 8 stages
+@export var hits_to_destroy: int = 1
 @export var can_be_destroyed: bool = true
+@export var allowed_to_spawn_powerup: bool = true
 @export var region_rect: Rect2 = Rect2(0, 0, 32, 16)
