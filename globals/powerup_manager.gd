94c03d823bff1782eab47dad224eaf538da1727d
Unain <vanstoel@gmail.com>
2023-10-09 19:01:22 +0200

    Changed Powerup Spawn Rate to weighted table

--- globals/powerup_manager.gd
+++ globals/powerup_manager.gd
@@ -1,18 +1,47 @@
 extends Node
 class_name PowerupManager
 
-const POWERUP_LIST: Array[PackedScene] = [
-	preload("res://scenes/powerups/powerup_enlarge.tscn"),
-	preload("res://scenes/powerups/powerup_disruption.tscn"),
-	preload("res://scenes/powerups/powerup_player.tscn"),
-	preload("res://scenes/powerups/powerup_breakout.tscn"),
-	preload("res://scenes/powerups/powerup_slow.tscn")
+var POWERUP_LIST: Array[Dictionary] = [
+	{
+		"scene": preload("res://scenes/powerups/powerup_enlarge.tscn"),
+		"roll_weight": 5.0,
+		"acc_weight": 0.0
+	},
+	{
+		"scene": preload("res://scenes/powerups/powerup_disruption.tscn"),
+		"roll_weight": 4.0,
+		"acc_weight": 0.0
+	},
+	{
+		"scene": preload("res://scenes/powerups/powerup_player.tscn"),
+		"roll_weight": 2.0,
+		"acc_weight": 0.0
+	},
+	{
+		"scene": preload("res://scenes/powerups/powerup_breakout.tscn"),
+		"roll_weight": 1.0,
+		"acc_weight": 0.0
+	},
+	{
+		"scene": preload("res://scenes/powerups/powerup_slow.tscn"),
+		"roll_weight": 5.0,
+		"acc_weight": 0.0
+	}
 ]
 
 var active_powerup: Powerup = null
 var powerup_on_screen: bool = false
+var powerup_total_weight: float = 0.0
 
 signal on_powerup_activated(powerup: Powerup)
+
+
+func _ready() -> void:
+	powerup_total_weight = 0.0
+	for powerup in POWERUP_LIST:
+		powerup_total_weight += powerup.roll_weight
+		powerup.acc_weight = powerup_total_weight
+
 
 func spawn_powerup(parent: Node2D, spawn_position: Vector2) -> void:
 	if powerup_on_screen: return
@@ -21,12 +50,26 @@ func spawn_powerup(parent: Node2D, spawn_position: Vector2) -> void:
 		return
 
 	if randi_range(1, 4) == 1:
-		var powerup: Powerup = POWERUP_LIST[randi() % POWERUP_LIST.size()].instantiate()
+		var powerup = _get_random_powerup()
+		if not is_instance_valid(powerup):
+			printerr("Couldn't get powerup from loot table")
+			return
+
 		powerup.global_position = spawn_position
 		powerup.on_screen_exited.connect(on_powerup_destroyed)
 		powerup.on_powerup_gained.connect(on_powerup_gained)
 		parent.add_child(powerup)
 		powerup_on_screen = true
+
+
+func _get_random_powerup() -> Powerup:
+	var roll: float = randf_range(0.0, powerup_total_weight)
+	print(roll)
+	for powerup in POWERUP_LIST:
+		if powerup.acc_weight >= roll:
+			return powerup.scene.instantiate() as Powerup
+
+	return null
 
 
 func on_powerup_gained(powerup: Powerup) -> void:
