5b7260dc791a08876fc2b4364674d6d6d8157e2a
Unain <vanstoel@gmail.com>
2023-10-10 18:30:29 +0200

    Added Laser Bullets

---
+++ scripts/laser.gd
@@ -0,0 +1,13 @@
+extends Area2D
+class_name Laser
+
+@export var speed = 300
+
+func _process(delta: float) -> void:
+	position += Vector2.UP * speed * delta
+
+
+func _on_collision(body: Node2D) -> void:
+	if body.has_method("on_collision"):
+		body.on_collision()
+	queue_free()
