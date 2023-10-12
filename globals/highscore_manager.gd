35afe0810e332128e094ca694fba8b8188380759
Unain <vanstoel@gmail.com>
2023-10-12 17:42:16 +0200

    Added Saving/Loading Highscore

--- globals/highscore_manager.gd
+++ globals/highscore_manager.gd
@@ -1,10 +1,38 @@
 extends Node
 
+const SAVEFILE: String = "user://highscore.sav"
+
 var current_score: int = 0
 var highscore: int = 0
+var new_highscore: bool = false
 
 signal on_score_updated
 signal on_highscore_updated
+
+
+func _ready() -> void:
+	_load_highscore()
+
+
+func _load_highscore() -> void:
+	if FileAccess.file_exists(SAVEFILE):
+		var file = FileAccess.open(SAVEFILE, FileAccess.READ)
+		if file:
+			highscore = file.get_64()
+			file.close()
+
+
+func save_highscore() -> void:
+	if new_highscore:
+		var file = FileAccess.open(SAVEFILE, FileAccess.WRITE)
+		if file:
+			file.store_64(highscore)
+			file.close()
+
+
+func reset_score() -> void:
+	current_score = 0
+	new_highscore = false
 
 
 func _add_points(points: int) -> void:
@@ -13,6 +41,7 @@ func _add_points(points: int) -> void:
 
 	if current_score > highscore:
 		highscore = current_score
+		new_highscore = true
 		on_highscore_updated.emit()
 
 
