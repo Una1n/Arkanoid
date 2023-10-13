2cb20540eef9778d36824e4544f73cef89713af0
Unain <vanstoel@gmail.com>
2023-10-13 15:51:42 +0200

    Added Keyboard/Controller support for paddle

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -4,16 +4,27 @@ class_name Paddle
 @onready var mouse_position_x: float = get_global_mouse_position().x
 @onready var size: int = 64
 
+var non_mouse_movement: bool = true
+var move_motion: Vector2 = Vector2.ZERO
+const MOVEMENT_SPEED: int = 10
 
 func _input(event: InputEvent) -> void:
-	# TODO: Keyboard + Controller
 	if event is InputEventMouseMotion:
 		mouse_position_x = get_global_mouse_position().x
+		non_mouse_movement = false
 
 
 func _physics_process(_delta: float) -> void:
-	var new_position: Vector2 = Vector2(mouse_position_x - global_position.x, 0)
-	move_and_collide(new_position)
+	var dir: float = Input.get_axis("move_left", "move_right")
+	if dir != 0:
+		non_mouse_movement = true
+
+	move_motion = Vector2(dir * MOVEMENT_SPEED, 0)
+
+	if move_motion == Vector2.ZERO and non_mouse_movement == false:
+		move_motion = Vector2(mouse_position_x - global_position.x, 0)
+
+	move_and_collide(move_motion)
 
 
 func normal_size() -> void:
